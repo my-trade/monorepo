519a64e1ccdec21de7c0ddfaef0bc590c821bb20
Bruno Basto <bruno.basto@liferay.com>
2019-10-15 11:12:48 -0300

    Remove

--- packages/osx-icon/src/index.js
+++
@@ -1,9 +0,0 @@
-import {JSXComponent} from 'metal-jsx';
-import App from './App';
-
-let root = document.createElement('div');
-
-root.id = 'root';
-document.body.appendChild(root);
-
-JSXComponent.render(<App />, document.getElementById('root'));
